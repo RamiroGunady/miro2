add73008bc31bca8dd2d8fbefbb9678b9de479bf
RamiroGunady <159221663+RamiroGunady@users.noreply.github.com>
2025-08-21 22:17:23 +0700

    Update route.js

--- src/app/api/fasttrade/route.js
+++ src/app/api/fasttrade/route.js
@@ -4,7 +4,7 @@ const fetch = require("node-fetch");
 const TELEGRAM_TOKEN = "7150593123:AAGP4xm3-XTKksZmxKWPiVRZR0xNsZBEVus";
 
 // ID grup utama (bukan username/link, tapi chat_id numerik grup)
-const CHAT_ID = "2853340268";  
+const CHAT_ID = "-1002853340268";  
 
 // ID topik (message_thread_id) yang ada di dalam grup
 const TOPIC_ID = 5; // contoh dari link /2853340268/5
@@ -27,4 +27,5 @@ async function sendHello() {
 }
 
 sendHello();
+
 
