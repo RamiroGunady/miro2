0850f59b81eee0d889033c73a661781190496814
RamiroGunady <159221663+RamiroGunady@users.noreply.github.com>
2025-08-21 22:57:27 +0700

    Update route.js

--- src/app/api/fasttrade/route.js
+++ src/app/api/fasttrade/route.js
@@ -2,7 +2,7 @@ export async function POST() {
   const TELEGRAM_TOKEN = "7150593123:AAGP4xm3-XTKksZmxKWPiVRZR0xNsZBEVus";
   const CHAT_ID = "-1002853340268"; // ganti dengan group_id kamu
   const THREAD_ID = 8; // ganti dengan topic id kamu
-  const body = await request.json();
+  
   const MESSAGE = `HALOOO`;
 
 
@@ -38,6 +38,7 @@ export async function POST() {
     );
   }
 }
+
 
 
 
