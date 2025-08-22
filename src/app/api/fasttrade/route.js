f900efa1134e30ef6b13f07603f2b9aa1ae8a83a
RamiroGunady <gunadyramiro@gmail.com>
2025-08-22 08:36:28 +0700

    fasttrade

--- src/app/api/fasttrade/route.js
+++ src/app/api/fasttrade/route.js
@@ -1,13 +1,14 @@
 // src/app/api/miro2/route.js
 export async function POST(request) {
   try {
-     const TELEGRAM_TOKEN = "7150593123:AAGP4xm3-XTKksZmxKWPiVRZR0xNsZBEVus";
-    const CHAT_ID = "-1002853340268"; // ganti dengan group_id kamu
-    const THREAD_ID = 8; // ganti dengan topic id kamu
-  // Ambil data JSON langsung
+    const TELEGRAM_TOKEN = "7150593123:AAGP4xm3-XTKksZmxKWPiVRZR0xNsZBEVus";
+    const CHAT_ID = "-1002736534446";
+
+    // Ambil data JSON langsung
     const body = await request.json();
-    const parsebody = JSON.parse(body)
-    const MESSAGE = `🚀 BUY`;
+
+    // Format pesan
+    const message = `🚀 ${body.condition} Signal - ${body.interval} 🚀\n\n${body.text} = ${body.price}`;
 
     // Kirim ke Telegram
     const telegramUrl = `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`;
@@ -16,12 +17,11 @@ export async function POST(request) {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         chat_id: CHAT_ID,
-      message_thread_id: THREAD_ID,
-        text: MESSAGE,
+        text: message,
         parse_mode: "Markdown"
       }),
     });
-    const data = await res.json();
+
     return new Response(
       JSON.stringify({
         success: true,
@@ -42,5 +42,3 @@ export async function POST(request) {
     );
   } 
 }
-
-
